c4eceabf8f845b839d365c61bad7d0fda05430a6
Alexandru Chimac <36740194+aL31cS@users.noreply.github.com>
2020-09-30 14:50:17 +0300

    Update check-patches.sh

--- Patches_When_Kernel_Source_Available/patches/check-patches.sh
+++ Patches_When_Kernel_Source_Available/patches/check-patches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
-git apply -v --check ../../device/manufacturer/codename/patches/0001-system_core.patch
+git apply -v --check ../../device/nokia/ne1/patches/0001-system_core.patch
 cd ../..
 cd hardware/interfaces
-git apply -v --check ../../device/manufacturer/codename/patches/0002-hardware_interfaces.patch
+git apply -v --check ../../device/nokia/ne1/patches/0002-hardware_interfaces.patch
 cd ../..
